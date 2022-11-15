2e61c81c045d13e5f119314b12e34099c97183fa
Blair <blair.liikala@gmail.com>
2022-11-15 12:11:13 -0600

    Version bump

--- addon.setup.php
+++ addon.setup.php
@@ -5,12 +5,12 @@ return [
     'author_url'        => 'blairliikala.com',
     'name'              => 'Dall-e AI Image Creation',
     'description'       => 'Create images from the AI Dall-E',
-    'version'           => '0.0.1',
+    'version'           => '0.0.2',
     'namespace'         => 'Blairliikala\Dalle',
     'settings_exist'    => true,
     'requires'          => [
         'php' => '8.0',
-        'ee'  => '7.1.6'
+        'ee'  => '7.2.0'
     ],
     'services' => [
         'api' => 'Services\Api',
@@ -23,15 +23,4 @@ return [
         'Settings' => 'Models\\Settings',
         'Errors' => 'Models\\Errors',
 	],
-    /* For Later.
-    'fieldtypes' => [
-        'mux_live' => [
-          'name' => 'Dalle Image',
-          'compatibility' => 'text',
-        ],
-    ],
-    'commands' => [
-        'mux:api' => 'Mux\Commands\Api_methods',
-    ]
-    */
 ];
